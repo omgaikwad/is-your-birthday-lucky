988bf3a20a9ea09bc8200d419cdf1bdc66c5e776
Om Gaikwad <62072438+omgaikwad@users.noreply.github.com>
2022-01-12 22:13:35 +0530

    updated code to ES6

--- app.js
+++ app.js
@@ -3,37 +3,31 @@ const luckyNumber = document.querySelector("#lucky-number");
 const checkButton = document.querySelector("#check-button");
 const outputDiv = document.querySelector("#output-div");
 
+checkButton.addEventListener("click", function isYourBirthdayLucky() {
+  const dob = dateOfBirth.value;
+  const luckyNumberValue = Number(luckyNumber.value);
+  const sum = calculateSum(dob);
 
-checkButton.addEventListener("click", function isYourBirthdayLucky(){
-    const dob = dateOfBirth.value;
-    const luckyNumberValue = Number(luckyNumber.value);
-    const sum = calculateSum (dob);
-
-    if(sum && luckyNumberValue){
-        if(sum % luckyNumberValue === 0){
-            // console.log("Your birthday is lucky");
-            outputDiv.innerText = "Your Birthday is lucky🚀";
-        }
-        else{
-            // console.log("Sorry your birthday is not lucky");
-            outputDiv.innerText = "Sorry, Your birthday is not that lucky😟";
-        }
+  if (sum && luckyNumberValue) {
+    if (sum % luckyNumberValue === 0) {
+      // console.log("Your birthday is lucky");
+      outputDiv.innerText = "Your Birthday is lucky🚀";
+    } else {
+      // console.log("Sorry your birthday is not lucky");
+      outputDiv.innerText = "Sorry, Your birthday is not that lucky😟";
     }
-    else{
-        outputDiv.innerText = "Please enter both the fields😡"
-    }
-    
-    
-
+  } else {
+    outputDiv.innerText = "Please enter both the fields😡";
+  }
 });
 
-function calculateSum(dob){
-    dob = dob.replaceAll("-", "");
-    
-    let sumOfBirthday = 0;
-    for(let i=0; i<dob.length; i++){
-        sumOfBirthday = sumOfBirthday + Number(dob[i]);
-    }
+const calculateSum = (dob) => {
+  dob = dob.replaceAll("-", "");
 
-    return sumOfBirthday;
-}
+  let sumOfBirthday = 0;
+  for (let i = 0; i < dob.length; i++) {
+    sumOfBirthday = sumOfBirthday + Number(dob[i]);
+  }
+
+  return sumOfBirthday;
+};
